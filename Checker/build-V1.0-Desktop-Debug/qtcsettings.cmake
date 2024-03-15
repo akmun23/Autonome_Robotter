6a15caec7070aaaac69dacc357a5a1db0ba350ff
PascalBjorstorp <pabjo23@student.sdu.dk>
2024-03-15 08:18:33 +0100

    Removed files

--- Checker/build-V1.0-Desktop-Debug/qtcsettings.cmake
+++
@@ -1,9 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/pascal/Documents/GitHub/Autonome_Robotter/Checker/build-V1.0-Desktop-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "/usr/bin/gcc" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/usr/bin/g++-11" CACHE "FILEPATH" "" FORCE)
