af134e65cc62cff52bc790403da7719932fcac5c
MadsHFromDK <159780248+MadsHFromDK@users.noreply.github.com>
2024-03-16 20:50:52 +0100

    New GUI V 2.0

---
+++ GUI/GUI VERSIONS/V2.0/build/CMakeFiles/Project_GUI.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_GUI.dir/main.cpp.o"
+  "CMakeFiles/Project_GUI.dir/main.cpp.o.d"
+  "Project_GUI"
+  "Project_GUI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_GUI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
