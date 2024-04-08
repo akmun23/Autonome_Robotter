9a5ef094c96424a53ff3b9cc9477d976a6598873
PascalBjorstorp <pabjo23@student.sdu.dk>
2024-04-08 14:19:26 +0200

    Seperat move table for player 1 og 2

--- Checker/V1.0/CheckersDatabase.h
+++ Checker/V1.0/CheckersDatabase.h
@@ -10,7 +10,7 @@
 void AddMove(int& TempBoardID, std::string& OldBoardState);
 void AddBoard(std::string& NewBoardState, std::string& OldBoardState, int& TempBoardID);
 void InsertToTemp(std::string& BoardState, std::string& Move, int& Counter, int PlayerId);
-void InsertToMove(int& BoardID, std::string& MoveToCheck, int& PlayerId);
+void InsertToMove(int& BoardID, std::string& MoveToCheck, int& PlayerId, std::string& PlayerMoveDB);
 void UpdateMoveWinrate(int& AmountOfMoves);
 std::string MovePlayer(std::string& BoardState,int& PlayerTurn);
 void RefreshTempTable(int& PlayerTurn);
