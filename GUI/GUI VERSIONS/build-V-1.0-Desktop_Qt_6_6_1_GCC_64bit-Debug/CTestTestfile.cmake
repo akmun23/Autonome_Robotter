03454809d6b87a5a4405a44cbef9e5078698cb5a
JonathanDalsgaard <157690382+RosaalbaL1@users.noreply.github.com>
2024-05-21 13:52:53 +0200

    Spaget

---
+++ GUI/GUI VERSIONS/build-V-1.0-Desktop_Qt_6_6_1_GCC_64bit-Debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aksel/Documents/GitHub/Autonome_Robotter/GUI/GUI VERSIONS/V 1.0
+# Build directory: /home/aksel/Documents/GitHub/Autonome_Robotter/GUI/GUI VERSIONS/build-V-1.0-Desktop_Qt_6_6_1_GCC_64bit-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
